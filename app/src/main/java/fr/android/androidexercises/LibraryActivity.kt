0a6dad4c00398064afddb92c8d53523a73dba013
Rémi Grison <rl.grison@gmail.com>
2018-11-15 15:28:49 +0100

    ajout dépendance vers Picasso et chargement image Gryffondor

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -2,6 +2,8 @@ package fr.android.androidexercises
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -11,6 +13,10 @@ class LibraryActivity : AppCompatActivity() {
 
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        var imageView = findViewById<ImageView>(R.id.downloadImageView)
+        Picasso.get()
+               .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+               .into(imageView)
     }
 
 }
